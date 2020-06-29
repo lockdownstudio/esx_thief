b58fb9be6f25da5dd183f50cad51da281030f4d1
Aljon Santiago <santiagoaj2001@gmail.com>
2020-06-29 10:49:38 +0800

    updated version of esx_thief

--- config.lua
+++ config.lua
@@ -5,5 +5,3 @@ Config.Locale = 'en'
 Config.EnableCash       = true
 Config.EnableBlackMoney = true
 Config.EnableInventory  = true
-Config.EnableItems      = true
-Config.EnableWeapons    = true
--- esx_thief.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `rare`, `can_remove`) VALUES
-    ('rope', 'rope', 0, 1)
-;
